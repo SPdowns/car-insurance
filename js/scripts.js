b5a63239c7919cc8f799cf3cf1bb53e0f350b852
Megan Hepner <megan.hepner@gmail.com>
2020-06-04 11:52:24 -0700

    add function lessons by Shawn, correct const to let > > Co-authored-by: Sean Downs <spdowns@protonmail.com> Co-authored-by: Megan Hepner <megan.hepner@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,6 @@ $(document).ready(function() {
   $("form#insurance").submit(function(event) {
     const age = parseInt($("input#age").val());
     const gender = $("select#gender").val();
-
-    // let quote = (100 - age) * 3;
-    // if (gender === 'male' && age < 26) {
-    //   quote += 50;
-    // }
     let result = makeQuote(gender, age);
 
     $("#rate").text(result);
@@ -20,25 +15,13 @@ $(document).ready(function() {
 // Back-End, Business Logic
 function makeQuote(gender, age) {
   let quote = (100 - age) * 3;
-  // if(gender === 'male' && age < 26) {
-  //   quote +=50;
-  // }
-  quote = calculateQuote(quote, gender, age);
+    if(gender === 'male' && age < 26) {
+      quote +=50;
+    }
+    console.log(quote)
+  // quote = calculateQuote(quote, gender, age);
   return quote;
 }
 
-function calculateQuote(quote, gender, age) {
-  if(gender === 'male' && age < 26) {
-    quote +=50;
-  }
-  return quote;
-}
 
-function recursiveFunction(index) {
-  if(index < 10) {
-    index++;
-    return recursiveFunction(index);
-  }
-  return index;
-}
 
